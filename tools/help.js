7940d189cb8f93e8c87fb0f001fcff396738fabc
lilcryptotech <lilcryptotech@gmail.com>
2023-12-07 19:33:07 -0500

    update help

--- tools/help.js
+++ tools/help.js
@@ -24,6 +24,7 @@ const HELP = {
         console.log("Welcome to the help program!");
         console.log("This program is the list for commands you can use to operate grabriel");
         console.log("=========================================================");
+        console.log("1) IP stock  Enter target ip address It will either public or private IP address");
         
             
      
